4635a29213ed9efdf9bc2130bd43c5b121cec1cc
Tasfia Addrita <tasfia.addrita@gmail.com>
2016-02-28 00:24:25 -0500

    fjieh

--- Twitter/ViewController.swift
+++ Twitter/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("hello")
+        print("hello")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
